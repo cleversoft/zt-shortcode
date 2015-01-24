fe84385938267b4ff8b01b7d0f35c3699ae8c340
Viet Vu <jooservices@gmail.com>
2015-01-24 15:25:24 +0700

    + add basic columns

---
+++ plugins/system/zo2shortcodes/core/html/column.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Zo2 Shortcodes (http://www.zo2framework.org)
+ *
+ * @link        http://www.zo2framework.org
+ * @link        http://github.com/aploss/zo2
+ * @author      ZooTemplate <http://zootemplate.com>
+ * @copyright   Copyright (c) 2013 APL Solutions (http://apl.vn)
+ * @license     GPL v2
+ */
+defined('_JEXEC') or die('Restricted access');
+?>
+<div class="col-md-<?php echo $options->get('md'); ?>"><?php echo $content; ?></div>
