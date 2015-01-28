37c4d81c3882e02fda169e9e132821b7c06ebe00
phuongnd <nguyenducphuong@phuongs-iMac.local>
2015-01-28 15:30:46 +0700

    update manifest

---
+++ plugins/system/zo2shortcodes/ztshortcodes.script.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * ZT Shortcodes
+ * A powerful Joomla plugin to help effortlessly customize your own content and style without HTML code knowledge
+ *
+ * @version     1.0.0
+ * @link        http://www.zootemplate.com
+ * @author      ZooTemplate 
+ * @copyright   Copyright (c) 2015 ZooTemplate
+ * @license     GPL v2
+ */
+
+defined('_JEXEC') or die();
+
+
+class PlgSystemZo2InstallerScript
+{
+    /**
+     * Called after any type of action
+     *
+     * @param     string              $route      Which action is happening (install|uninstall|discover_install)
+     * @param     jadapterinstance    $adapter    The object responsible for running this script
+     *
+     * @return    boolean                         True on success
+     */
+    public function postFlight($route, JAdapterInstance $adapter)
+    {
+        $db    = JFactory::getDBO();
+        $query = $db->getQuery(true);
+        $query
+            ->update('#__extensions')
+            ->set("`enabled`='1'")
+            ->where("`type`='plugin'")
+            ->where("`folder`='system'")
+            ->where("`element`='ztshortcodes'");
+        $db->setQuery($query);
+        $db->execute();
+        
+        return true;
+    }
+}
