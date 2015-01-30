ed9e39c215b5a8c2501c398c4376b0cb3ec22e28
Viet Vu <jooservices@gmail.com>
2015-01-30 16:13:10 +0700

    ^ update gallery support styles

--- plugins/system/ztshortcodes/core/html/gallery.php
+++ plugins/system/ztshortcodes/core/html/gallery.php
@@ -1,30 +1,12 @@
 <?php
-$dir = JPath::clean(JPATH_ROOT . '/images/' . $options->get('dir'));
 
-// Allowed filetypes
-$allowedExtensions = array('jpg', 'png', 'gif');
-// Also allow filetypes in uppercase
-$allowedExtensions = array_merge($allowedExtensions, array_map('strtoupper', $allowedExtensions));
-// Build the filter. Will return something like: "jpg|png|JPG|PNG|gif|GIF"
-$filter = implode('|', $allowedExtensions);
-$filter = "^.*\.(" . implode('|', $allowedExtensions) . ")$";
-$files = JFolder::files($dir, $filter, false, true);
-
-foreach ($files as $key => $image)
+$style = $options->get('style');
+$styleFile = __DIR__ . '/galleries/' . $style . '.php';
+if (JFile::exists($styleFile))
 {
-    $image = JPath::clean($image);
-    $images[$key]['src'] = ZtShortcodesPath::getInstance()->toUrl($image);
-    $images[$key]['thumbnail'] = ZtShortcodesPath::getInstance()->toUrl(ZtShortcodesHelperImage::getThumbnail($image, $options));
+    require $styleFile;
+} else // Use default file if asked file is not exists
+{
+    __DIR__ . '/galleries/default.php';
 }
 ?>
-<?php if (!empty($images)) : ?>
-    <?php foreach ($images as $image) : ?>
-        <a href="<?php echo $image['src']; ?>" class="ztshortcodes-gallery">
-            <img src="<?php echo $image['thumbnail']; ?>"></a>
-    <?php endforeach; ?>
-<?php endif; ?>
-<script>
-    jQuery(document).ready(function () {
-        jQuery('a.ztshortcodes-gallery').colorbox({});
-    })
-</script>
