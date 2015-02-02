64f4699ab2fa882d2e144b3f6aa8d88ed7c5e01d
Viet Vu <jooservices@gmail.com>
2015-02-02 10:25:53 +0700

    + button plugin

---
+++ plugins/editors-xtd/ztshortcodes/ztshortcodes.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * ZT Shortcodes
+ * A powerful Joomla plugin to help effortlessly customize your own content and style without HTML code knowledge
+ *
+ * @version     1.0.0
+ * @author      ZooTemplate 
+ * @email       support@zootemplate.com 
+ * @link        http://www.zootemplate.com 
+ * @copyright   Copyright (c) 2015 ZooTemplate
+ * @license     GPL v2
+ */
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * 
+ */
+class PlgButtonZtShortcodes extends JPlugin
+{
+
+    /**
+     * Load the language file on instantiation.
+     *
+     * @var    boolean
+     * @since  3.1
+     */
+    protected $autoloadLanguage = true;
+
+    /**
+     * Display the button.
+     *
+     * @param   string   $name    The name of the button to display.
+     * @param   string   $asset   The name of the asset being edited.
+     * @param   integer  $author  The id of the author owning the asset being edited.
+     *
+     * @return  array    A two element array of (imageName, textToInsert) or false if not authorised.
+     */
+    public function onDisplay($name, $asset, $author)
+    {
+        $app = JFactory::getApplication();
+
+        $extension = $app->input->get('option');
+
+        if ($asset == '')
+        {
+            $asset = $extension;
+        }
+
+
+        $link = 'index.php?ztshortcodes_task=display&ztshortcodes_view=shortcodes';
+        JHtml::_('behavior.modal');
+        $button = new JObject;
+        $button->modal = true;
+        $button->class = 'btn';
+        $button->link = $link;
+        $button->text = JText::_('PLG_EDITORS-XTD_ZTSHORTCODES_BUTTON');
+        $button->name = 'picture';
+        $button->options = "{handler: 'iframe', size: {x: 800, y: 500}}";
+
+        return $button;
+    }
+
+}
