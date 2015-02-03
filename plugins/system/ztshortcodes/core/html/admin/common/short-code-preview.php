4e77de460285fe697827ed15d9b5e65bde7ffa4b
Dung Tran <tad88.dev@gmail.com>
2015-02-03 17:56:46 +0700

    + add shortcode common controls

---
+++ plugins/system/ztshortcodes/core/html/admin/common/short-code-preview.php
@@ -0,0 +1,11 @@
+<div id="zo2-shortcode-preview">
+    <div class="form-group clearfix">
+        <label for="zo2-sc-value">Shortcode Content</label>
+        <textarea placeholder="Shortcode Content" rows="3" class="form-control"
+                  id="zo2-sc-value"></textarea>
+    </div>
+    <div class="form-group clearfix">
+        <label for="zo2-sc-render">Shortcode Preview</label>
+        <div id="zo2-sc-render" class="shortcode-render"></div>
+    </div>
+</div>
