769a00621bb81e621c06505a927984d28eec954d
Viet Vu <jooservices@gmail.com>
2015-02-09 15:11:12 +0700

    ^ gen id from php server side

--- plugins/system/ztshortcodes/core/html/zt_accordion_title.php
+++ plugins/system/ztshortcodes/core/html/zt_accordion_title.php
@@ -11,22 +11,24 @@
  * @license     GPL v2
  */
 defined('_JEXEC') or die('Restricted access');
+$titleId = 'zt-title-' . md5(microtime());
+$contentId = 'zt-content-' . md5(microtime());
 ?>
 <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="<?php echo $options->get('title-id'); ?>">
+    <div class="panel-heading" role="tab" id="<?php echo $titleId; ?>">
         <h4 class="panel-title">
             <a 
                 data-toggle="collapse" 
                 data-parent="#<?php echo $options->get('parent'); ?>"
-                href="#<?php echo $options->get('content-id'); ?>"
+                href="#<?php echo $contentId; ?>"
                 aria-expanded="<?php echo $options->get('expanded'); ?>" 
-                aria-controls="<?php echo $options->get('content-id'); ?>">
+                aria-controls="<?php echo $contentId; ?>">
                     <?php echo $options->get('title'); ?>
             </a>
         </h4>
     </div>
-    <div id="<?php echo $options->get('content-id'); ?>"
-         class="panel-collapse collapse<?php echo ' '.$options->get('active'); ?>" role="tabpanel" aria-labelledby="<?php echo $options->get('title-id'); ?>">
+    <div id="<?php echo $contentId; ?>"
+         class="panel-collapse collapse<?php echo ' ' . $options->get('active'); ?>" role="tabpanel" aria-labelledby="<?php echo $titleId; ?>">
         <div class="panel-body">
             <?php echo $content; ?>
         </div>
