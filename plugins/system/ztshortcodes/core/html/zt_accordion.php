73dae9b42ff808fcb1b9ff8da40803b1caeb4fda
Viet Vu <jooservices@gmail.com>
2015-02-09 16:31:51 +0700

    ^ update accordion id

--- plugins/system/ztshortcodes/core/html/zt_accordion.php
+++ plugins/system/ztshortcodes/core/html/zt_accordion.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die('Restricted access');
 ?>
 <div 
     class="<?php echo $options->get('class'); ?>"
-    id="<?php echo 'zt-' . md5(microtime()); ?>" 
+    id="<?php echo 'zt-accordion-' . md5(microtime()); ?>" 
     role="<?php echo $options->get('role'); ?>" 
     aria-multiselectable="<?php echo $options->get('aria-multiselectable'); ?>">
 
