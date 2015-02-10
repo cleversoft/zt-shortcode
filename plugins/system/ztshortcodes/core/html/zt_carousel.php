7bda200bf03ca7170db9747aa4c8eddab5e81751
Viet Vu <jooservices@gmail.com>
2015-02-11 00:18:18 +0700

    ^ update carousel & sample

--- plugins/system/ztshortcodes/core/html/zt_carousel.php
+++ plugins/system/ztshortcodes/core/html/zt_carousel.php
@@ -1,15 +1,20 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: chinhbeo
- * Date: 2/5/15
- * Time: 8:45 AM
+ * ZT Shortcodes
+ * A powerful Joomla plugin to help effortlessly customize your own content and style without HTML code knowledge
+ *
+ * @version     1.0.0
+ * @author      ZooTemplate 
+ * @email       support@zootemplate.com 
+ * @link        http://www.zootemplate.com 
+ * @copyright   Copyright (c) 2015 ZooTemplate
+ * @license     GPL v2
  */
 defined('_JEXEC') or die('Restricted access');
 ?>
 
 <div class="carousel-wrap">
-    <div class="carousel-slider" id="carousel-<?php echo $options->get('id'); ?>"
+    <div class="carousel-slider" id="<?php echo ZtShortcodesHelperCommon::getUniqueString('zt-carousel-'); ?>"
          data-items="<?php echo $options->get('showItem'); ?>"
          data-duration="<?php echo $options->get('duration'); ?>"
          data-responsinve="<?php echo $options->get('enableReponsive'); ?>">
@@ -28,10 +33,13 @@ defined('_JEXEC') or die('Restricted access');
         echo $parser->getAsHTML();
         ?>
     </div>
-    <?php if($options->get('showControl') == "true"): ?>
-    <div class="prev-btn"><i class="fa fa-chevron-left"></i></div>
-    <div class="next-btn"><i class="fa fa-chevron-right"></i></div>
-    <?php endif; if($options->get('showPager') == "true"): ?>
-    <div class="pager"></div>
+    <?php if ($options->get('showControl') == "true"): ?>
+        <div class="prev-btn"><i class="fa fa-chevron-left"></i></div>
+        <div class="next-btn"><i class="fa fa-chevron-right"></i></div>
+        <?php
+    endif;
+    if ($options->get('showPager') == "true"):
+        ?>
+        <div class="pager"></div>
     <?php endif; ?>
 </div>
--- plugins/system/ztshortcodes/core/html/zt_carousel_item.php
+++ plugins/system/ztshortcodes/core/html/zt_carousel_item.php
@@ -1,27 +1,34 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: chinhbeo
- * Date: 2/5/15
- * Time: 11:16 AM
+ * ZT Shortcodes
+ * A powerful Joomla plugin to help effortlessly customize your own content and style without HTML code knowledge
+ *
+ * @version     1.0.0
+ * @author      ZooTemplate 
+ * @email       support@zootemplate.com 
+ * @link        http://www.zootemplate.com 
+ * @copyright   Copyright (c) 2015 ZooTemplate
+ * @license     GPL v2
  */
 defined('_JEXEC') or die('Restricted access');
-
 ?>
 
 <div class="carousel-item">
     <div class="carousel-item-inner">
         <img src="<?php echo $options->get('image'); ?>" alt="carousel"/>
-        <?php if($options->get('link') != '' || $options->get('title') != ''): ?>
-        <div class="image-extras">
-            <div class="image-extras-content">
-                <?php if($options->get('link') != '') : ?>
-                <a href="<?php echo $options->get('link'); ?>" class="icon-link"><i class="fa fa-link"></i></a>
-                <?php endif; if($options->get('title') != ''): ?>
-                <h3><a href="<?php echo $options->get('link'); ?>"><?php echo $options->get('title'); ?></a></h3>
-                <?php endif; ?>
+        <?php if ($options->get('link') != '' || $options->get('title') != ''): ?>
+            <div class="image-extras">
+                <div class="image-extras-content">
+                    <?php if ($options->get('link') != '') : ?>
+                        <a href="<?php echo $options->get('link'); ?>" class="icon-link"><i class="fa fa-link"></i></a>
+                            <?php
+                        endif;
+                        if ($options->get('title') != ''):
+                            ?>
+                        <h3><a href="<?php echo $options->get('link'); ?>"><?php echo $options->get('title'); ?></a></h3>
+                    <?php endif; ?>
+                </div>
             </div>
-        </div>
         <?php endif; ?>
     </div>
 </div>
