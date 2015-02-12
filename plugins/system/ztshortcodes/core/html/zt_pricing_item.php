12ca75a0059aff3be8bdb4cc17edc0d35d14e8b9
Viet Vu <jooservices@gmail.com>
2015-02-12 12:09:17 +0700

    ^ prepare price values

--- plugins/system/ztshortcodes/core/html/zt_pricing_item.php
+++ plugins/system/ztshortcodes/core/html/zt_pricing_item.php
@@ -11,24 +11,34 @@
  * @copyright   Copyright (c) 2015 ZooTemplate
  * @license     GPL v2
  */
-
 defined('_JEXEC') or die('Restricted access');
 
 $html = $classHightLight = '';
 $price = explode("|", $options->get('price'));
-
-if($options->get('item-hightlight') == true){
-        $classHightLight = 'recommended';
+if (!isset($price[0]))
+{
+    $price[0] = '';
+}
+if (!isset($price[1]))
+{
+    $price[1] = '';
+}
+if (!isset($price[2]))
+{
+    $price[2] = '';
+}
+if ($options->get('item-hightlight') == true)
+{
+    $classHightLight = 'recommended';
 }
 
-$html .= '<div class="pricing-item pull-left"><div class="plan'. ' '.$options->get('level-item') .''. ' '. $classHightLight .'">';
-$html .= '<div class="head"><h2>'. $options->get('title') .'</h2></div>';
-$html .= '<ul class="item-list">'. $content .'</ul>';
-$html .= '<div class="price"><h3><span class="symbol">'. $price[0] .'</span>'. $price[1] .'</h3><h4>'. $price[2] .'</h4></div>';
-$html .= '<a class="btn btn-success" href="'. $options->get('link') .'">'. $options->get('button-text') .'</a>';
+$html .= '<div class="pricing-item pull-left"><div class="plan' . ' ' . $options->get('level-item') . '' . ' ' . $classHightLight . '">';
+$html .= '<div class="head"><h2>' . $options->get('title') . '</h2></div>';
+$html .= '<ul class="item-list">' . $content . '</ul>';
+$html .= '<div class="price"><h3><span class="symbol">' . $price[0] . '</span>' . $price[1] . '</h3><h4>' . $price[2] . '</h4></div>';
+$html .= '<a class="btn btn-success" href="' . $options->get('link') . '">' . $options->get('button-text') . '</a>';
 $html .= '</div></div>';
 
 echo $html;
-
 ?>
 
