84f42e58c5ba3626fd4ccc32572159ba57f150e6
chinhbeo <chinhlv91@gmail.com>
2015-02-24 16:20:26 +0700

    ^ update shortcode button & block quote

--- plugins/system/ztshortcodes/core/html/admin/zt_blockquotes.php
+++ plugins/system/ztshortcodes/core/html/admin/zt_blockquotes.php
@@ -15,29 +15,35 @@
 $prefix = 'zt-sc-';
 ?>
 
-<form>
-    <div class="form-group clearfix">
-        <label for="<?php echo $prefix.'blockquotes-type'; ?>">Type</label>
-        <select id="<?php echo $prefix.'blockquotes-type'; ?>" class="form-control">
-            <option value="">Default</option>
-            <option value="big">Big Quote</option>
-            <option value="box">Box Quote</option>
-        </select>
+<div id="<?php echo $prefix . 'container'; ?>" data-tag="" data-root="true">
+    <div id="<?php echo $prefix . 'container'; ?>" data-tag="zt_blockquotes">
+        <div class="form-group clearfix">
+            <label for="<?php echo $prefix . 'blockquotes-type'; ?>">Type</label>
+            <select id="<?php echo $prefix . 'blockquotes-type'; ?>" class="form-control sc-selectbox" data-property="type">
+                <option value="">Default</option>
+                <option value="big">Big Quote</option>
+                <option value="box">Box Quote</option>
+            </select>
+        </div>
+        <div class="form-group clearfix">
+            <label for="<?php echo $prefix . 'blockquotes-author'; ?>">Author</label>
+            <input type="text" data-property="author" class="form-control sc-textbox" id="<?php echo $prefix . 'blockquotes-author'; ?>"
+                   placeholder="Enter Author Blockquotes">
+        </div>
+        <div class="form-group clearfix">
+            <label for="<?php echo $prefix . 'blockquotes-author-link'; ?>">Author Link</label>
+            <input type="text" data-property="author-link" class="form-control sc-textbox" id="<?php echo $prefix . 'blockquotes-author-link'; ?>"
+                   placeholder="Enter Author Link Blockquotes">
+        </div>
+        <div class="form-group clearfix">
+            <label for="<?php echo $prefix . 'blockquotes-content'; ?>">Content</label>
+            <textarea placeholder="Content Blockquotes Box" rows="3" class="form-control sc-textbox"
+                      id="<?php echo $prefix . 'blockquotes-content'; ?>" data-property="">Content Blockquotes Box</textarea>
+        </div>
+        <div class="form-group clearfix">
+            <label for="<?php echo $prefix . 'blockquotes-class'; ?>">Extra Class</label>
+            <input type="text" class="form-control sc-textbox" id="<?php echo $prefix . 'blockquotes-class'; ?>"
+                   placeholder="Enter Extra Class Blockquotes" data-property="extra-class">
+        </div>
     </div>
-    <div class="form-group clearfix">
-        <label for="<?php echo $prefix.'blockquotes-author'; ?>">Author</label>
-        <input type="text" class="form-control" id="<?php echo $prefix.'blockquotes-author'; ?>" placeholder="Enter Author Blockquotes">
-    </div>
-    <div class="form-group clearfix">
-        <label for="<?php echo $prefix.'blockquotes-author-link'; ?>">Author Link</label>
-        <input type="text" class="form-control" id="<?php echo $prefix.'blockquotes-author-link'; ?>" placeholder="Enter Author Link Blockquotes">
-    </div>
-    <div class="form-group clearfix">
-        <label for="<?php echo $prefix.'blockquotes-content'; ?>">Content</label>
-        <textarea placeholder="Content Blockquotes Box" rows="3" class="form-control" id="<?php echo $prefix.'blockquotes-content'; ?>">Content Blockquotes Box</textarea>
-    </div>
-    <div class="form-group clearfix">
-        <label for="<?php echo $prefix.'blockquotes-class'; ?>">Extra Class</label>
-        <input type="label" class="form-control" id="<?php echo $prefix.'blockquotes-class'; ?>" placeholder="Enter Extra Class Blockquotes">
-    </div>
-</form>
+</div>
