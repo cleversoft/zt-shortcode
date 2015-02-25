2f7e6acfa818faa98cbfb2727bc5a6f39347350a
chinhbeo <chinhlv91@gmail.com>
2015-02-25 09:57:20 +0700

    ^ update shortcode pricing table

--- plugins/system/ztshortcodes/core/html/admin/zt_pricing.php
+++ plugins/system/ztshortcodes/core/html/admin/zt_pricing.php
@@ -12,54 +12,56 @@
  * @license     GPL v2
  */
 
-$prefix = 'zt-sc-pricing-';
+$prefix = 'zt-sc-';
 ?>
 
-<form>
-    <div class="form-group clearfix">
-        <label for="<?php echo $prefix . 'type'; ?>">Pricing Type</label>
-        <select id="<?php echo $prefix . 'type'; ?>" class="form-control">
-            <option value="">Default</option>
-            <option value="attached">Attached</option>
-        </select>
-    </div>
-    <div id="<?php echo $prefix . 'container' ?>">
-        <div id="<?php echo $prefix . 'element' ?>">
-            <div class="form-group clearfix">
-                <div class="form-group">
-                    <label for="<?php echo $prefix . 'title' ?>">Pricing Title</label>
-                    <input type="text" class="form-control" id="<?php echo $prefix . 'title' ?>"
+<div id="<?php echo $prefix . 'container'; ?>" data-tag="zt_pricing" data-root="true">
+    <div id="<?php echo $prefix . 'container'; ?>" data-tag="zt_pricing">
+        <div class="form-group clearfix">
+            <label for="<?php echo $prefix . 'pricing-type'; ?>">Pricing Type</label>
+            <select id="<?php echo $prefix . 'pricing-type'; ?>" class="form-control sc-selectbox" data-property="pricing-type">
+                <option value="">Default</option>
+                <option value="attached">Attached</option>
+            </select>
+        </div>
+        <div id="<?php echo $prefix . 'container'; ?>" data-tag="zt_pricing" data-root="true">
+            <div id="<?php echo $prefix . 'container'; ?>" data-tag="zt_pricing_item">
+                <div class="form-group clearfix">
+                    <label for="<?php echo $prefix . 'pricing-title' ?>">Pricing Title</label>
+                    <input type="text" class="form-control sc-textbox" data-property="title" id="<?php echo $prefix . 'pricing-title' ?>"
                            placeholder="Enter Title Pricing" value="Title Pricing">
                 </div>
-                <div class="checkbox">
+                <div class="checkbox form-group clearfix">
                     <label>
-                        <input type="checkbox" id="<?php echo $prefix . 'hightlight'; ?>"> Item Hightlight
+                        <input type="checkbox" class="sc-checkbox" data-property="item-hightlight" id="<?php echo $prefix . 'pricing-hightlight'; ?>"> Item Hightlight
                     </label>
                 </div>
-                <div class="form-group">
-                    <label for="<?php echo $prefix . 'content' ?>">Content</label>
-                    <textarea placeholder="Content Pricing" rows="3" class="form-control"
-                              id="<?php echo $prefix . 'content' ?>">Content Pricing</textarea>
+                <div class="form-group clearfix">
+                    <label for="<?php echo $prefix . 'pricing-content' ?>">Content</label>
+                    <textarea placeholder="Content Pricing" rows="3" class="form-control sc-textbox" data-property=""
+                              id="<?php echo $prefix . 'pricing-content' ?>">Content Pricing</textarea>
                 </div>
-                <div class="form-group">
-                    <label for="<?php echo $prefix . 'content' ?>">Price</label>
-                    <input type="text" class="form-control" id="<?php echo $prefix . 'price' ?>"
+                <div class="form-group clearfix">
+                    <label for="<?php echo $prefix . 'pricing-price' ?>">Price</label>
+                    <input type="text" class="form-control sc-textbox" data-property="price" id="<?php echo $prefix . 'pricing-price' ?>"
                            placeholder="Enter Price (exp: $|90|per month).">
                 </div>
-                <div class="form-group">
-                    <label for="<?php echo $prefix . 'link' ?>">Link</label>
-                    <input type="text" class="form-control" id="<?php echo $prefix . 'link' ?>"
+                <div class="form-group clearfix">
+                    <label for="<?php echo $prefix . 'pricing-link' ?>">Link</label>
+                    <input type="text" class="form-control sc-textbox" data-property="link" id="<?php echo $prefix . 'pricing-link' ?>"
                            placeholder="Enter Link Pricing" value="#">
                 </div>
-                <div class="form-group">
-                    <label for="<?php echo $prefix . 'button-text' ?>">Button Text</label>
-                    <input type="text" class="form-control" id="<?php echo $prefix . 'button-text' ?>"
+                <div class="form-group clearfix">
+                    <label for="<?php echo $prefix . 'pricing-button-text' ?>">Button Text</label>
+                    <input type="text" class="form-control sc-textbox" data-property="button-text" id="<?php echo $prefix . 'pricing-button-text' ?>"
                            placeholder="Enter Button Text" value="Button Text">
                 </div>
             </div>
         </div>
+        <div class="form-group clearfix">
+            <button class="btn btn-default" type="button" id="<?php echo $prefix . 'new-pricing-table' ?>">Add New
+                Pricing
+            </button>
+        </div>
     </div>
-    <div class="form-group clearfix">
-        <button class="btn btn-default" type="button" id="<?php echo $prefix . 'new-pricing-table' ?>">Add New Pricing</button>
-    </div>
-</form>
+</div>
