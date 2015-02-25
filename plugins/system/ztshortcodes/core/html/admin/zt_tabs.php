d2a9f376bf154a56f2840c0cf961db17c6881e78
chinhbeo <chinhlv91@gmail.com>
2015-02-25 09:01:05 +0700

    ^ update shortcode tabs

--- plugins/system/ztshortcodes/core/html/admin/zt_tabs.php
+++ plugins/system/ztshortcodes/core/html/admin/zt_tabs.php
@@ -15,24 +15,31 @@
 $prefix = 'zt-sc-';
 ?>
 
-<div id="<?php echo $prefix . 'tabs-container' ?>">
-    <div id="<?php echo $prefix . 'tabs-element' ?>">
-        <div class="form-group clearfix">
-            <label for="<?php echo $prefix . 'tab-title' ?>">Title</label>
-            <input type="text" class="form-control" id="<?php echo $prefix . 'tab-title' ?>" placeholder="Enter title">
+<div id="<?php echo $prefix . 'container'; ?>" data-tag="zt_tabs" data-root="true">
+    <div id="<?php echo $prefix . 'container'; ?>" data-tag="zt_tabs">
+        <div id="<?php echo $prefix . 'container'; ?>" data-tag="" data-root="true">
+            <div id="<?php echo $prefix . 'container'; ?>" data-tag="zt_tab">
+                <div class="form-group clearfix">
+                    <label for="<?php echo $prefix . 'tab-title' ?>">Title</label>
+                    <input type="text" class="form-control sc-textbox" data-property="title"
+                           id="<?php echo $prefix . 'tab-title' ?>"
+                           placeholder="Enter title">
+                </div>
+                <div class="form-group clearfix">
+                    <label for="<?php echo $prefix . 'tab-content'; ?>">Content Tab</label>
+                    <textarea placeholder="Content Tabs" rows="3" class="form-control sc-textbox" data-property=""
+                              id="<?php echo $prefix . 'tab-content'; ?>">This is tab content</textarea>
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" class="sc-" id="<?php echo $prefix . 'tab-active'; ?>"> Active
+                    </label>
+                </div>
+            </div>
         </div>
+
         <div class="form-group clearfix">
-            <label for="<?php echo $prefix . 'tab-content'; ?>">Content Tab</label>
-            <textarea placeholder="Content Tabs" rows="3" class="form-control"
-                      id="<?php echo $prefix . 'tab-content'; ?>">This is tab content</textarea>
-        </div>
-        <div class="checkbox">
-            <label>
-                <input type="checkbox" id="<?php echo $prefix . 'tab-active'; ?>"> Active
-            </label>
+            <button class="btn btn-default" type="button" id="<?php echo $prefix . 'new-tab' ?>">Add New Tab</button>
         </div>
     </div>
-</div>
-<div class="form-group clearfix">
-    <button class="btn btn-default" type="button" id="<?php echo $prefix . 'new-tab' ?>">Add New Tab</button>
 </div>
