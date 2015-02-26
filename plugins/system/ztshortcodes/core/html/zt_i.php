ce6f1a24c4c334cfe3ec01ed51f387fabf6ab831
chinhbeo <chinhlv91@gmail.com>
2015-02-26 11:14:08 +0700

    ^ update shortcode Testimonial

--- plugins/system/ztshortcodes/core/html/zt_i.php
+++ plugins/system/ztshortcodes/core/html/zt_i.php
@@ -14,21 +14,23 @@
 ?>
 
 <?php
-    $html = $style = $class = '';
-    if($options->get('iconCircle') == "yes"){
-        $style = 'style="background-color: '. $options->get('iconBgColor') .'; color: '. $options->get('iconColor') .'; border-color: '. $options->get('iconBdColor') .'"';
-        $class .= 'ic-circle';
-    } else{
+$html = $style = $class = '';
+if($options->get('iconCircle') == "yes"){
+    $style = 'style="background-color: '. $options->get('iconBgColor') .'; color: '. $options->get('iconColor') .'; border-color: '. $options->get('iconBdColor') .'"';
+    $class .= 'ic-circle';
+} else{
+    if($options->get('iconColor')){
         $style = 'style="color: '. $options->get('iconColor') .'"';
     }
-    $class .= ' ic-'.$options->get('iconSize').' '.$options->get('classIcon');
-    if($options->get('spinningIcon') == "yes"){
-        $class .= ' fa-spin';
-    }
-    if($content != ''){
-        $class .= ' ic-list';
-    }
+}
+$class .= ' ic-'.$options->get('iconSize').' '.$options->get('classIcon');
+if($options->get('spinningIcon') == "yes"){
+    $class .= ' fa-spin';
+}
+if($content != ''){
+    $class .= ' ic-list';
+}
 
-    $html .= '<i class="zt-font-ic  '. $class .'" '. $style .'>'. $content .'</i>';
-    echo $html;
+$html .= '<i class="zt-font-ic  '. $class .'" '. $style .'></i>'. $content .'';
+echo $html;
 ?>
