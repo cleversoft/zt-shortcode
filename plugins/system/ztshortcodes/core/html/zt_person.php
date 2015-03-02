db324405b1dbfc6b490614fb18561e7fbd2f44c4
chinhbeo <chinhlv91@gmail.com>
2015-03-02 13:41:06 +0700

    ^ Update Shortcode Perons

--- plugins/system/ztshortcodes/core/html/zt_person.php
+++ plugins/system/ztshortcodes/core/html/zt_person.php
@@ -12,33 +12,40 @@
  * @license     GPL v2
  */
 
-if($options->get('tooltip') == "yes"){
+if ($options->get('tooltip') == "yes") {
     $toolTip = 'data-toggle="tooltip"';
 }
 ?>
 
-<div class="zt-person">
+<div class="zt-person col-sm-<?php echo $options->get('column'); ?> col-md-<?php echo $options->get('column'); ?>">
     <div class="zt-person-image">
         <img alt="John Doe" src="<?php echo $options->get('image'); ?>"
-             class="img-responsive">
+             class="img-responsive img-<?php echo ($options->get('img-type')) ? $options->get('img-type') : 'default'; ?>">
     </div>
-    <div class="zt-person-desc">
-        <div class="zt-person-author">
+    <div class="zt-person-details">
+        <div class="zt-person-author pull-left">
             <span class="person-name"><?php echo $options->get('name'); ?></span>
             <span class="person-position"><?php echo $options->get('position'); ?></span>
-            <div class="zt-person-social">
-                <ul>
-                    <?php if($options->get('facebook')) { ?>
-                    <li><a href="<?php echo $options->get('facebook'); ?>" <?php echo $toolTip; ?> data-placement="top" title="Facebook"></a> </li>
-                    <?php } ?>
-                    <?php if($options->get('twitter')) { ?>
-                        <li><a href="<?php echo $options->get('twitter'); ?>" <?php echo $toolTip; ?> data-placement="top" title="Twitter"></a> </li>
-                    <?php } ?>
-                    <?php if($options->get('mail')) { ?>
-                        <li><a href="<?php echo $options->get('mail'); ?>" <?php echo $toolTip; ?> data-placement="top" title="Email"></a> </li>
-                    <?php } ?>
-                </ul>
-            </div>
+        </div>
+        <div class="zt-person-social pull-right social-<?php echo ($options->get('social-type')) ? $options->get('social-type') : 'default'; ?>">
+            <ul>
+                <?php if ($options->get('facebook')) { ?>
+                    <li><a href="<?php echo $options->get('facebook'); ?>"><i <?php echo $toolTip; ?>
+                                data-placement="top" title="Facebook" class="fa fa-facebook"></i></a></li>
+                <?php } ?>
+                <?php if ($options->get('twitter')) { ?>
+                    <li><a href="<?php echo $options->get('twitter'); ?>" <?php echo $toolTip; ?> data-placement="top"
+                           title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                <?php } ?>
+                <?php if ($options->get('email')) { ?>
+                    <li><a href="<?php echo $options->get('email'); ?>" <?php echo $toolTip; ?> data-placement="top"
+                           title="Email"><i class="fa fa-envelope-o"></i></a></li>
+                <?php } ?>
+                <?php if ($options->get('google')) { ?>
+                    <li><a href="<?php echo $options->get('google'); ?>" <?php echo $toolTip; ?> data-placement="top"
+                           title="Google Plus"><i class="fa fa-google-plus"></i></a></li>
+                <?php } ?>
+            </ul>
         </div>
         <div class="zt-person-content clearfix"><?php echo $content; ?></div>
     </div>
