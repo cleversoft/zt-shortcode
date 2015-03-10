4beec49126a9bc2f4e07c7a65409374e534e7615
Dung Tran <tad88.dev@gmail.com>
2015-03-10 11:06:04 +0700

    + Add common controls event listener

--- plugins/system/ztshortcodes/core/assets/js/shortcode.js
+++ plugins/system/ztshortcodes/core/assets/js/shortcode.js
@@ -8,7 +8,39 @@
 
     /* Short code main class */
     var _shortcode = {
+        _elements: {
+            /* Control button */
+            comButtons: "#zt-shortcode-controls",
+            buttonInsert: "#zt-sc-insert",
+            buttonPreview: "#zt-sc-preview",
+            buttonClose: "#zt-sc-close",
+            /* Shortcode preview */
+            comPreview: "#zt-shortcode-preview",
+            shortcodeBbcode: "#zt-sc-bbcode",
+            shortcodeRender: "#zt-sc-render"
+        },
         _init: function(){
+            var _self = this;
+            /* Insert button */
+            $(_self._elements.comButtons).on('click', _self._elements.buttonInsert, function () {
+                var code = $(_self._elements.shortcodeBbcode).val();
+                if (typeof (w.parent) !== 'undefined') {
+                    if (w.parent.hasOwnProperty('jInsertEditorText')) {
+                        /* Insert to parent editor */
+                        w.parent.jInsertEditorText(code);
+                        /* Close the box */
+                        w.parent.SqueezeBox.close();
+                    }
+                }
+            });
+            /* Close button */
+            $(_self._elements.comButtons).on('click', _self._elements.buttonClose, function () {
+                if (typeof (w.parent) !== 'undefined') {
+                    if (w.parent.hasOwnProperty('SqueezeBox')) {
+                        w.parent.SqueezeBox.close();
+                    }
+                }
+            });
         }
     };
     /* Check for Zo2 javascript framework */
