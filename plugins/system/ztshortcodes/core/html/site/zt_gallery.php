f607df1ddeaf5fd08ba818140dabf281a983383f
chinhbeo <chinhlv91@gmail.com>
2015-03-12 16:28:46 +0700

    ^ Update shortcode gallery and video

--- plugins/system/ztshortcodes/core/html/site/zt_gallery.php
+++ plugins/system/ztshortcodes/core/html/site/zt_gallery.php
@@ -14,11 +14,57 @@
 
 $style = $attributes->get('style');
 $styleFile = __DIR__ . '/galleries/' . $style . '.php';
-if (JFile::exists($styleFile))
-{
+if (JFile::exists($styleFile)) { ?>
     require $styleFile;
-} else // Use default file if asked file is not exists
-{
-    __DIR__ . '/galleries/default.php';
-}
-?>
+<?php } else { ?>
+
+    <?php
+    $dir = JPath::clean(JPATH_ROOT . '/images/' . $attributes->get('dir'));
+    // Allowed filetypes
+    $allowedExtensions = array('jpg', 'png', 'gif');
+    // Also allow filetypes in uppercase
+    $allowedExtensions = array_merge($allowedExtensions, array_map('strtoupper', $allowedExtensions));
+    // Build the filter. Will return something like: "jpg|png|JPG|PNG|gif|GIF"
+    $filter = implode('|', $allowedExtensions);
+    $filter = "^.*\.(" . implode('|', $allowedExtensions) . ")$";
+    $files = JFolder::files($dir, $filter, false, true);
+    if (is_array($files))
+    {
+    foreach ($files as $key => $image)
+    {
+    $image = JPath::clean($image);
+    $images[$key]['src'] = ZtShortcodesPath::getInstance()->toUrl(
+    ZtShortcodesHelperImage::getThumbnail($image, $attributes->get('width', 600), $attributes->get('height', 600), 'resized_')
+    );
+    $images[$key]['thumbnail'] = ZtShortcodesPath::getInstance()->toUrl(
+    ZtShortcodesHelperImage::getThumbnail($image, $attributes->get('thumb-width', 300), $attributes->get('thumb-height', 300), 'thumb_')
+    );
+    }
+    }
+
+    $style = '';
+    if($attributes->get('thumb-width') != ''){
+    $style .= 'width: '. $attributes->get('thumb-width') . 'px';
+    } elseif($attributes->get('thumb-height') != ''){
+    $style .= 'height: '. $attributes->get('thumb-height') .'px';
+    }
+    ?>
+
+    <div class="zt-gallery zt-gallery-default clearfix">
+        <?php if (!empty($images)) : ?>
+            <?php foreach ($images as $image) : ?>
+                <a href="<?php echo $image['src']; ?>" class="ztshortcodes-gallery group1">
+                    <img src="<?php echo $image['thumbnail']; ?>" alt="Image Gallery" style="<?php echo $style; ?>"></a>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
+    <script>
+        jQuery(document).ready(function () {
+            jQuery('a.ztshortcodes-gallery').colorbox({
+                width: <?php echo $attributes->get('width'); ?>,
+                height: <?php echo $attributes->get('height'); ?>,
+                rel: "group1"
+            });
+        })
+    </script>
+<?php } ?>
--- plugins/system/ztshortcodes/core/html/site/zt_video_play.php
+++ plugins/system/ztshortcodes/core/html/site/zt_video_play.php
@@ -17,18 +17,18 @@
 
     // Function call video Link both youtube and vimeo, now
     if (!function_exists('zo2VideoEmbed')) {
-        function zo2VideoEmbed($url, $width = 640, $height = 480) {
+        function ztVideoEmbed($url, $width = 640, $height = 480) {
             if (strpos($url,'youtube') || strpos($url,'youtu.be')){
-                return zo2VideoYoutube($url, $width, $height);
+                return ztVideoYoutube($url, $width, $height);
             } else {
-                return zo2VideoVimeo($url, $width, $height);
+                return ztVideoVimeo($url, $width, $height);
             }
         }
     }
 
     // Function process video youtube
     if (!function_exists('zo2VideoYoutube')) {
-        function zo2VideoYoutube($url, $width = 640, $height = 480) {
+        function ztVideoYoutube($url, $width = 640, $height = 480) {
             preg_match('~(?:https?://)?(?:www.)?(?:youtube.com|youtu.be)/(?:watch\?v=)?([^\s]+)~', $url, $video_id);
             return '<iframe itemprop="video" src="http://www.youtube.com/embed/'. $video_id[1] .'?wmode=transparent" width="'. $width .'" height="'. $height .'" ></iframe>';
         }
@@ -36,11 +36,11 @@
 
     // Function process video vimeo
     if (!function_exists('zo2VideoVimeo')) {
-        function zo2VideoVimeo($url, $width = 640, $height = 480) {
+        function ztVideoVimeo($url, $width = 640, $height = 480) {
             preg_match('/https?:\/\/(?:www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/', $url, $video_id);
             return '<iframe itemprop="video" src="http://player.vimeo.com/video/'. $video_id[3] .'?title=0&amp;byline=0&amp;portrait=0?wmode=transparent" width="'. $width .'" height="'. $height .'"></iframe>';
         }
     }
 
-    echo zo2VideoEmbed($attributes->get('src'), $attributes->get('width'), $attributes->get('height'));
+    echo ztVideoEmbed($attributes->get('src'), $attributes->get('width'), $attributes->get('height'));
 ?>
