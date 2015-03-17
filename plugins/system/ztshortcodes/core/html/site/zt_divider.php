8ff107ec5adf4f20a050110a9a4e65de62dbd84d
chinhbeo <chinhlv91@gmail.com>
2015-03-17 09:11:30 +0700

    ^ Update shortcode divider

--- plugins/system/ztshortcodes/core/html/site/zt_divider.php
+++ plugins/system/ztshortcodes/core/html/site/zt_divider.php
@@ -14,29 +14,35 @@
 ?>
 
 <?php
-    $html = $classTop = '';
-    if($attributes->get('type') == 'text-only'){
-        $classTop = 'go-to-top';
-    } else if($attributes->get('type') == 'icon-type-1'){
-        $classTop = 'go-to-top-icon-1';
-    } else if($attributes->get('type') == 'icon-type-2'){
-        $classTop = 'go-to-top-icon-2';
-    }
+$color = $attributes->get('color');
+$type = $attributes->get('type');
+$text = $attributes->get('text');
+$icon = $attributes->get('icon');
+$html = $classTop = $style = '';
+if ($attributes->get('type') == 'text-only') {
+    $classTop = 'go-to-top';
+} else if ($attributes->get('type') == 'icon-type-1') {
+    $classTop = 'go-to-top-icon-1';
+} else if ($attributes->get('type') == 'icon-type-2') {
+    $classTop = 'go-to-top-icon-2';
+}
 
-    $html .= '<div class="zt-divider-wrap">';
-    $html .= '<div class="zt-divider '. $classTop .' divider-'. $attributes->get('type') .'">';
-    if($attributes->get('type') == 'text-only' || $attributes->get('type') == 'icon-type-1' || $attributes->get('type') == 'icon-type-2'){
-        $html .= '<a href="#" class="animate-top">';
-        if($attributes->get('type') == 'text-only'){
-            $html .= $attributes->get('text');
-        } else if($attributes->get('type') == 'icon-type-1'){
-            $html .= '<i class="'. $attributes->get('icon') .'"></i>';
-        } else {
-            $html .= $attributes->get('text').' <i class="'. $attributes->get('icon') .'"></i>';
-        }
-        $html .= '</a>';
-    }
-    $html .= '</div>';
-    $html .= '</div>';
-    echo $html;
 ?>
+
+<div class="zt-divider-wrap">
+    <div class="zt-divider <?php echo $classTop; ?> divider-<?php echo $type; ?>"
+         <?php if ($type == "hr") { ?>style="background-color: <?php echo $color; ?>" <?php } ?>
+        <?php if($type == "thin" || $type == "fat" || $type == "dotted" || $type == "icon-type-1" || $type == "icon-type-2") { ?>style="border-color: <?php echo $color; ?>"<?php } ?>>
+        <?php if ($attributes->get('type') == 'text-only' || $attributes->get('type') == 'icon-type-1' || $attributes->get('type') == 'icon-type-2') { ?>
+            <a href="#" class="animate-top" <?php if($type == "text-only") { ?>style="border-color: <?php echo $color; ?>"<?php } ?>>
+                <?php if ($attributes->get('type') == 'text-only') { ?>
+                    <?php echo $text; ?>
+                <?php } else if ($attributes->get('type') == 'icon-type-1') { ?>
+                    <i class="<?php echo $icon; ?>"></i>
+                <?php } else { ?>
+                    <?php echo $text; ?><i class="<?php echo $icon; ?>"></i>';
+                <?php } ?>
+            </a>
+        <?php } ?>
+    </div>
+</div>
