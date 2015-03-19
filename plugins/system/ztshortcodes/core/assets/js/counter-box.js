02200f76c5d8f13631a445bf9ba43a594ceb6f8a
chinhbeo <chinhlv91@gmail.com>
2015-03-19 09:08:41 +0700

    ^ Update shortcode counter Box

--- plugins/system/ztshortcodes/core/assets/js/counter-box.js
+++ plugins/system/ztshortcodes/core/assets/js/counter-box.js
@@ -6,35 +6,34 @@
  */
 
 
-var counter = jQuery('.zt-count-asset').find('.count-number');
-jQuery(counter).each(function (){
-    var d = jQuery(this).data('speed'),
-        f = jQuery(this).data('from'),
-        t = jQuery(this).data('to'),
-        u = jQuery(this).data('updown')
 
-    if(u == 'up'){
-        jQuery(this).counter({
-            autoStart: false,
-            duration: d,
-            countFrom: f,
-            countTo: t,
-            placeholder: 0,
-            easing: "easeOutCubic"
-        });
-    } else {
-        jQuery(this).counter({
-            autoStart: false,
-            countTo: t,
-            duration: 7000,
-            easing: "easeOutCubic"
-        });
-    }
-});
-jQuery(window).load(function(){
-    jQuery('.counter-box-wrap').each(function(){
-        jQuery().waypoint && jQuery(this).waypoint(function () {
-            counter.counter("start");
+jQuery(document).ready(function(){
+    var counter = jQuery('.zt-count-asset').find('.count-number');
+    jQuery(counter).each(function(){
+        var d = jQuery(this).data('speed'),
+            f = jQuery(this).data('from'),
+            t = jQuery(this).data('to'),
+            u = jQuery(this).data('updown')
+
+
+        jQuery(this).waypoint(function () {
+            if(u == 'up'){
+                jQuery(this).counter({
+                    autoStart: true,
+                    duration: d,
+                    countFrom: f,
+                    countTo: t,
+                    placeholder: 0,
+                    easing: "easeOutCubic"
+                });
+            } else {
+                jQuery(this).counter({
+                    autoStart: true,
+                    countTo: t,
+                    duration: 7000,
+                    easing: "easeOutCubic"
+                });
+            }
         }, {triggerOnce: !0, offset: "bottom-in-view"});
     });
 });
