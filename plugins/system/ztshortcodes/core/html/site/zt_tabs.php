a97ea0b6e3ca1eac809052c60d2accadbc1d8700
chinhbeo <chinhlv91@gmail.com>
2015-03-19 10:10:55 +0700

    ^ Update shortcode tabs

--- plugins/system/ztshortcodes/core/html/site/zt_tabs.php
+++ plugins/system/ztshortcodes/core/html/site/zt_tabs.php
@@ -13,40 +13,62 @@
 defined('_JEXEC') or die('Restricted access');
 global $ztShortcodes;
 if (isset($ztShortcodes['tabs'][$attributes->get('id')])) :
-    $tabs = $ztShortcodes['tabs'][$attributes->get('id')];
+$tabs = $ztShortcodes['tabs'][$attributes->get('id')];
 
 
 ?>
-<div role="tabpanel" class="zt-tabs zt-tab-<?php echo $attributes->get('type');  ?>">
-<ul
-    class="nav nav-tabs">
-    <?php foreach ($tabs as $key => $tab) :
-        $tabkey = str_replace(" ", "-", $tab['attributes']->get('title'));
-        $tabhref = strtolower($tabkey);
+<div role="tabpanel" class="zt-tabs zt-tab-<?php echo $attributes->get('type'); ?>">
+    <?php if($attributes->get('type') != 'bottom-tabs') : ?>
+    <ul
+        class="nav nav-tabs">
+        <?php foreach ($tabs as $key => $tab) :
+            $tabkey = str_replace(" ", "-", $tab['attributes']->get('title'));
+            $tabhref = strtolower($tabkey);
+            ?>
+            <li class="<?php echo ($tab['attributes']->get('active') == 'yes') ? 'active' : ''; ?>">
+                <a
+                    href="#<?php echo $tabhref; ?>"
+                    data-toggle="tab">
+                    <?php echo $tab['attributes']->get('title'); ?>
+                </a>
+            </li>
+        <?php
+        endforeach;
         ?>
-        <li class="<?php echo ($tab['attributes']->get('active') == 'yes') ? 'active' : ''; ?>">
-            <a
-                href="#<?php echo $tabhref; ?>"
-                data-toggle="tab">
-                <?php echo $tab['attributes']->get('title'); ?>
-            </a>
-        </li>
-    <?php
-    endforeach;
-    ?>
-</ul>
-<div
-    class="tab-content"
-    >
-    <?php foreach ($tabs as $key => $tab) :
-        $tabkey = str_replace(" ", "-", $tab['attributes']->get('title'));
-        $tabhref = strtolower($tabkey);
+    </ul>
+    <?php endif; ?>
+    <div class="tab-content">
+        <?php foreach ($tabs as $key => $tab) :
+            $tabkey = str_replace(" ", "-", $tab['attributes']->get('title'));
+            $tabhref = strtolower($tabkey);
+            ?>
+            <div class="tab-pane fade <?php echo ($tab['attributes']->get('active') == 'yes') ? 'active in' : ''; ?>"
+                 id="<?php echo $tabhref; ?>">
+                <?php echo $tab['content']; ?>
+            </div>
+        <?php
+        endforeach;
         ?>
-        <div class="tab-pane fade <?php echo ($tab['attributes']->get('active') == 'yes') ? 'active in' : ''; ?>" id="<?php echo $tabhref; ?>">
-            <?php echo $tab['content']; ?>
-        </div>
-    <?php
-    endforeach;
-    ?>
+    </div>
+    <?php if($attributes->get('type') == 'bottom-tabs') : ?>
+        <ul
+            class="nav nav-tabs">
+            <?php foreach ($tabs as $key => $tab) :
+                $tabkey = str_replace(" ", "-", $tab['attributes']->get('title'));
+                $tabhref = strtolower($tabkey);
+                ?>
+                <li class="<?php echo ($tab['attributes']->get('active') == 'yes') ? 'active' : ''; ?>">
+                    <a
+                        href="#<?php echo $tabhref; ?>"
+                        data-toggle="tab">
+                        <?php echo $tab['attributes']->get('title'); ?>
+                    </a>
+                </li>
+            <?php
+            endforeach;
+            ?>
+        </ul>
+    <?php endif; ?>
 </div>
+
 <?php endif; ?>
